8fa1294adb243d09ab66d60318b7f7df3f38e53e
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-10-10 22:42:14 +0100

    We are done here

---
+++ index.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT, ()=>{ console.log(`App running on :${process.env.PORT}`); })
+app.get('/', (req, res)=>{ return res.status(200).json({ ok : true, message : "OK", status : 200 }) });
